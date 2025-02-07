9733bdf58cb8910c7cb5b9fcf422f53751c06ada
grace <110770777+graceemlin@users.noreply.github.com>
2025-02-06 20:58:00 -0800

    Update friend.go

--- bot/friend.go
+++ bot/friend.go
@@ -10,10 +10,12 @@ import (
 	"strings"
 )
 
-func friend(action int, username string, id string, client *http.Client, message *discordgo.MessageCreate, session *discordgo.Session) {
+func friend(action int, username string, id string, message *discordgo.MessageCreate, session *discordgo.Session) {
+	// construct urls
 	friendURL := "https://www.101weiqi.com/u/" + username + "/"
 	attionURL := "https://www.101weiqi.com/attionuser/"
-	
+
+	// get friend profile
 	friend_get_response, friend_get_error := client.Get(friendURL)
 	if friend_get_error != nil {
 		log.Fatal("Error fetching login page:", friend_get_error)
@@ -24,17 +26,19 @@ func friend(action int, username string, id string, client *http.Client, message
 		log.Fatalf("Status code error: %d %s", 	friend_get_response.StatusCode, friend_get_response.Status)
 	}
 
+	// find csrftoken
 	var csrftoken string
 	for _, cookie := range friend_get_response.Cookies() {
 		if cookie.Name == "csrftoken" {
 			csrftoken = cookie.Value
 		}
 	}
-
+	
 	if csrftoken == "" {
 		log.Fatal("csrftoken not found on friend's profile page", nil)
 	}
 
+	// parse friend profile html
 	friend_html, friend_html_error := goquery.NewDocumentFromReader(friend_get_response.Body)
 	if friend_html_error != nil {
 		log.Fatal("Error parsing HTML:", friend_html_error)
@@ -53,6 +57,7 @@ func friend(action int, username string, id string, client *http.Client, message
 		"csrfmiddlewaretoken": {csrfmiddlewaretoken},
 	}
 
+	// construct friend POST request
 	friend_post_request, friend_post_error := http.NewRequest(http.MethodPost, attionURL, strings.NewReader(formData.Encode()))
 	if friend_post_error != nil {
 		log.Fatal("Error creating friend POST request:", friend_post_error)
@@ -63,13 +68,14 @@ func friend(action int, username string, id string, client *http.Client, message
 	friend_post_request.Header.Set("Cache-Control", "no-cache")
 	friend_post_request.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	friend_post_request.Header.Set("Origin", "https://www.101weiqi.com")
+	friend_post_request.Header.Set("Referer", "https://www.101weiqi.com/")
 	friend_post_request.Header.Set("Pragma", "no-cache")
 	friend_post_request.Header.Set("Priority", "u=1, i")
-	friend_post_request.Header.Set("Referer", "https://www.101weiqi.com/")
 	friend_post_request.Header.Set("Sec-Fetch-Mode", "cors")
 	friend_post_request.Header.Set("Sec-Fetch-Site", "same-origin")
 	friend_post_request.Header.Set("X-Requested-With", "XMLHttpRequest")
-	
+
+	// send friend POST request
 	friend_post_response, friend_post_request_error := client.Do(friend_post_request)
 	if friend_post_request_error != nil {
                 log.Fatal("Friend POST request failed.")           
