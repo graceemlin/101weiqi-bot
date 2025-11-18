302a34e32d8110c5d31c59a7ba5e2deae4676d3b
grace <110770777+graceemlin@users.noreply.github.com>
2025-11-18 12:06:19 -0500

    Update login.go

--- bot/login.go
+++ bot/login.go
@@ -49,32 +49,21 @@ func login() {
 	// create form data for login POST
 	formData := url.Values{
 		"csrfmiddlewaretoken": {csrfmiddlewaretoken},
-		"source":              {"index_nav"},
-		"form_username":       {USERNAME},
-		"form_password":       {PASSWORD},
+		"username":            {USERNAME},
+		"password":            {PASSWORD},
 	}
 
 	// construct login POST request
-	login_post_request, login_post_request_creation_error := http.NewRequest(http.MethodPost, LOGINURL, strings.NewReader(formData.Encode()))
+	login_post_request, login_post_request_creation_error := http.NewRequest(http.MethodPost, LOGINWQURL, strings.NewReader(formData.Encode()))
 	if login_post_request_creation_error != nil {
 		log.Fatal("Error creating login POST request:", login_post_request_creation_error)
 	}
 
-	login_post_request.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
-	login_post_request.Header.Set("Accept-Encoding", "gzip, deflate, br, zstd")
-	login_post_request.Header.Set("Accept-Language", "en-US,en;q=0.9")
-	login_post_request.Header.Set("Cache-Control", "no-cache")
+	login_post_request.Header.Set("Accept", "application/json, text/plain, */*")
 	login_post_request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	login_post_request.Header.Set("Origin", "https://www.101weiqi.com")
-	login_post_request.Header.Set("Pragma", "no-cache")
-	login_post_request.Header.Set("Priority", "u=0, i")
-	login_post_request.Header.Set("Referer", "https://www.101weiqi.com/")
-	login_post_request.Header.Set("Sec-Ch-Ua-Mobile", "?0")
-	login_post_request.Header.Set("Sec-Fetch-Dest", "document")
-	login_post_request.Header.Set("Sec-Fetch-Mode", "navigate")
-	login_post_request.Header.Set("Sec-Fetch-Site", "same-origin")
-	login_post_request.Header.Set("Sec-Fetch-User", "?1")
-	login_post_request.Header.Set("Upgrade-Insecure-Requests", "1")
+	login_post_request.Header.Set("Referer", "https://www.101weiqi.com/login/")
+	login_post_request.Header.Set("X-Requested-With", "XMLHttpRequest")
 
 	// send login POST request
 	login_post_response, login_post_error := client.Do(login_post_request)
@@ -83,11 +72,16 @@ func login() {
 	}
 	defer login_post_response.Body.Close()
 
-	if login_post_response.Request.URL.String() == HOMEURL {
+	login_successful := false
+	for _, cookie := range login_post_response.Cookies() {
+		if cookie.Name == "sessionid" {
+			login_successful = true
+		}
+	}
+
+	if login_successful {
 		fmt.Println("Successfully logged in!")
-	} else if login_post_response.Request.URL.String() == LOGINURL {
-		log.Fatal("Failed to login. Check your username/password")
 	} else {
-		log.Fatal("Failed to login. Possible throttle on POSTs from this account")
+		log.Fatal("Failed to login. Possible throttle on POSTs from this account.")
 	}
 }
